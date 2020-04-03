3377b89b63cdfc1cf1590fac01d0fcf1b8461bed
Sebastian Pietschner <sebastian.pietschner@gmail.com>
2020-04-03 19:40:46 +1100

    basic routing and structure

--- src/App.js
+++ src/App.js
@@ -1,26 +1,36 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import Fourohfour from './pages/Fourohfour';
+import Home from './pages/Home';
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+          </ul>
+        </nav>
+
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route>
+            <Fourohfour />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
-
-export default App;
---
+++ src/pages/Fourohfour/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default class Fourohfour extends React.Component {
+    render() {
+      return <h1>This is the 404 page</h1>;
+    }
+  }
