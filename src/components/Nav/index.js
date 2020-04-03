3bace621ba6f6879aa453d8f52edae9b5fcb1eb5
Sebastian Pietschner <sebastian.pietschner@gmail.com>
2020-04-03 21:12:15 +1100

    template and dir structure

---
+++ src/components/Nav/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default class Nav extends React.Component { 
+    return() {
+        return(<h1>Nav</h1>)
+    }
+}
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -2,6 +2,54 @@ import React from 'react';
 
 export default class Home extends React.Component {
     render() {
-      return <h1>This is the home page</h1>;
+      return(<section class="hero is-primary is-medium">
+      <div class="hero-head">
+        <nav class="navbar">
+          <div class="container">
+            <div class="navbar-brand">
+              <a class="navbar-item">
+                <img src="https://bulma.io/images/bulma-type-white.png" alt="Logo"/>
+              </a>
+              <span class="navbar-burger burger" data-target="navbarMenuHeroA">
+                <span></span>
+                <span></span>
+                <span></span>
+              </span>
+            </div>
+            <div id="navbarMenuHeroA" class="navbar-menu">
+              <div class="navbar-end">
+                <a class="navbar-item is-active">
+                  Home
+                </a>
+                <a class="navbar-item">
+                  Examples
+                </a>
+                <a class="navbar-item">
+                  Documentation
+                </a>
+                <span class="navbar-item">
+                  <a class="button is-primary is-inverted">
+                    <span class="icon">
+                      <i class="fab fa-github"></i>
+                    </span>
+                    <span>Download</span>
+                  </a>
+                </span>
+              </div>
+            </div>
+          </div>
+        </nav>
+      </div>
+      <div class="hero-body">
+        <div class="container has-text-centered">
+          <h1 class="title">
+            Title
+          </h1>
+          <h2 class="subtitle">
+            Subtitle
+          </h2>
+        </div>
+      </div>
+    </section>);
     }
   }
